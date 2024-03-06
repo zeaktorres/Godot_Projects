923c01cb3be7761a89b52085cb0f9d3eec67ddbb
zeaktorres <torreszeaks@gmail.com>
2024-03-06 11:59:51 -0800

    Added moving zombie which atttempts to attack player. More fine tuning may be required.

--- SpookyGame/Scripts/Zombie.gd
+++ SpookyGame/Scripts/Zombie.gd
@@ -1,22 +1,25 @@
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
-@export var SPEED = 5
 
-var target_velocity = Vector3.ZERO
+
+
+@onready var target =$"../Child"
+@onready var nav_agent = $"Pivot/Zombie/NavigationAgent3D" 
+@onready var animation_tree= $"Pivot/Zombie/AnimationTree"
+@export var SPEED = 3
+
 var direction = Vector3.ZERO
+var state_machine
 
-@onready var target = $"../NavigationRegion3D/Target"
-@onready var nav_agent = $Pivot/Character/NavigationAgent3D
-@onready var camera = $"../Camera3D"
-@onready var animation_tree = $Pivot/Character/AnimationTree
+enum {WALKING, IDLE}
 
-enum {RUNNING, WALKING, IDLE}
-
+func _ready():
+	state_machine = animation_tree.get("parameters/playback")
 
 func _process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	velocity = Vector3.ZERO
+			
 
 	nav_agent.target_position = target.position
 	var next_nav_point = nav_agent.get_next_path_position()
@@ -27,17 +30,17 @@ func _process(delta):
 	$Pivot.basis = Basis.looking_at(direction)
 	
 	
-	# TODO: Handle the delta usecase?
+	
+	
 	velocity.x = direction.x * SPEED
 	velocity.z = direction.z * SPEED
-		
-	if (global_transform.origin.distance_to(target.position) < 1.5):
-		velocity = Vector3.ZERO
-		
-	animation_tree.set("parameters/conditions/isRunning", getState() == RUNNING)
-	animation_tree.set("parameters/conditions/isWalking", getState() == WALKING)
-	animation_tree.set("parameters/conditions/isIdle", getState() == IDLE)
 	
+	if (global_transform.origin.distance_to(target.position) < 1.5):
+		animation_tree.set("parameters/conditions/isClose", true)
+	else:
+		animation_tree.set("parameters/conditions/isClose", false)
+	if state_machine.get_current_node() == "Attack1":
+		velocity = Vector3.ZERO
 
 	# Moving the Character
 	move_and_slide()
@@ -48,5 +51,3 @@ func getState():
 		return IDLE
 	elif (magnitude < 4):
 		return WALKING
-	else:
-		return RUNNING
