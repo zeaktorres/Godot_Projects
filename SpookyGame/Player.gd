fce40e0f46a7b83453f209461a0c812df9734e83
zeaktorres <torreszeaks@gmail.com>
2024-03-05 17:09:15 -0800

    Adding AI to SpookyGame.

--- SpookyGame/Player.gd
+++ SpookyGame/Player.gd
@@ -1,34 +1,35 @@
 extends CharacterBody3D
 
 # How fast the player moves in meters per second.
-@export var speed = 7
+@export var SPEED = 4
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
 var target_velocity = Vector3.ZERO
 var direction = Vector3.ZERO
 
+@onready var target = $"../NavigationRegion3D/Target"
+@onready var nav_agent = $Pivot/Character/NavigationAgent3D
+var nav_loaded: bool = false
 
-func _physics_process(delta):
+
+func _process(delta):
 	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("left", "right", "forward", "down")
-	var look_at_direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
-	direction = direction.lerp(look_at_direction.normalized(), delta * 2)
+	velocity = Vector3.ZERO
+	nav_agent.target_position = target.position
+	var next_nav_point = nav_agent.get_next_path_position()
+	var move_to_direction = next_nav_point - global_transform.origin
+	var look_at_direction = Vector3(move_to_direction.x, 0, move_to_direction.z)
+	direction = direction.lerp(look_at_direction.normalized(), delta * 10)
+	direction = direction.normalized()
+	$Pivot.basis = Basis.looking_at(direction)
+	print()
 	
 	
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$Pivot.basis = Basis.looking_at(direction)
-	if is_on_floor():
-		if direction:
-			velocity.x = direction.x * speed
-			velocity.z = direction.z * speed
-		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
-		
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
 	
-	if look_at_direction.x == 0 && look_at_direction.z == 0:
+	if (global_transform.origin.distance_to(target.position) < 2):
 		velocity = Vector3.ZERO
 		
 	if (velocity.length() > 0):
