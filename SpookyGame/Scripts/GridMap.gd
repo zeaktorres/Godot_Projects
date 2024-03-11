ab60d3f26ccec92ba1dffcf1024d2f85d83c363b
zeaktorres <torreszeaks@gmail.com>
2024-03-11 12:18:24 -0700

    Added some avoidence.

--- SpookyGame/Scripts/GridMap.gd
+++ SpookyGame/Scripts/GridMap.gd
@@ -4,7 +4,7 @@ extends GridMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(get_used_cells())
+	print(map_to_local(get_used_cells()[0]))
 
 	
 	pass # Replace with function body.
@@ -12,13 +12,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if (Zombies.get_child_count() > 0):
-		print(isInsideCell(get_used_cells()[0], Zombies.get_children()[0]))
 	pass
 	
 func isInsideCell(cell, zombie):
-	print(cell.x, zombie.position.x)
-	print()
 	
 	if (cell.x - 0.5 > zombie.position.x && cell.x + 0.5 > zombie.position.x):
 		return true
--- SpookyGame/Scripts/Zombie.gd
+++ SpookyGame/Scripts/Zombie.gd
@@ -13,6 +13,7 @@ var player
 
 var direction = Vector3.ZERO
 var state_machine
+var intended_velocity
 
 enum {RUNNING, WALKING, IDLE}
 
@@ -24,8 +25,6 @@ func _process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	velocity = Vector3.ZERO
 
-	# Navigation Agent
-	
 	# Move zombie
 	nav_agent.set_target_position(player.position)
 	var next_nav_point = nav_agent.get_next_path_position()
@@ -35,12 +34,15 @@ func _process(delta):
 			animation_tree.advance(delta * 1.5)
 			pass
 		_:
-			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+			intended_velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+			nav_agent.set_velocity(intended_velocity)
 	
 	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
 	if (nav_agent.is_target_reached()):
-		velocity = Vector3.ZERO
+		intended_velocity = Vector3.ZERO
 		animation_tree.set("parameters/conditions/isAttacking", true)
+		print("Here")
 	else:
 		# Determine what state the player is in
 		animation_tree.set("parameters/conditions/isAttacking", false)
@@ -52,7 +54,7 @@ func _process(delta):
 	move_and_slide()
 	
 func getState():
-	var magnitude = velocity.length()
+	var magnitude = intended_velocity.length()
 	if (magnitude == 0):
 		return IDLE
 	elif (magnitude < 4):
@@ -66,4 +68,10 @@ func _hit_finished():
 
 
 func _on_area_3d_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity):
+	velocity = safe_velocity
+	move_and_slide()
 	pass # Replace with function body.
